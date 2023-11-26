3212f6501c01f71f8c895c03826742f82f3d18f4
matecky <152031086+matecky@users.noreply.github.com>
2023-11-26 21:20:34 +0100

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,35 +1,32 @@
-import { webcrack } from "webcrack"
-import { Deobfuscator } from "deobfuscator"
+import { REstringer } from "restringer"
 import { writeFile } from "node:fs/promises"
 import { assert } from "node:console"
 
-async function deobfuscationLoop(obfuscatedInput, loopFunction) {
-    let deobfuscated = obfuscatedInput
-    for (let run = 0; run < 5; run++) {
-        try {
-            const result = await loopFunction(deobfuscated)
-            if (result == "" || result == undefined) break
-            deobfuscated = result
-        } catch (e) {
-            console.error(e)
-            break
-        }
-    }
-    return deobfuscated
-}
+//async function deobfuscationLoop(obfuscatedInput, loopFunction) {
+//    let deobfuscated = obfuscatedInput
+//    for (let run = 0; run < 5; run++) {
+//        try {
+//            const result = await loopFunction(deobfuscated)
+//            if (result == "" || result == undefined) break
+//            deobfuscated = result
+//        } catch (e) {
+//            console.error(e)
+//            break
+//        }
+//    }
+//    return deobfuscated
+//}
 
-async function deobfuscationChain(obfuscatedScript, deobfsSteps) {
+async function deobfuscationx(obfuscatedScript) {
     let deobfs = obfuscatedScript
-    for(const func of deobfsSteps) {
-        deobfs = await deobfuscationLoop(deobfs, func)
-    }
-    return deobfs
+    deobfs = await new REstringer(deobfs)
+    return deobfs.script
 }
 
-const synchrony = new Deobfuscator()
-const webcrackStep = async (x) => await webcrack(x).code
-const synchronyStep = async (x) => await synchrony.deobfuscateSource(x)
-const checkDeobfs = (x) => x.indexOf("encrypted") !== -1
+//const synchrony = new Deobfuscator()
+//const webcrackStep = async (x) => await webcrack(x).code
+//const synchronyStep = async (x) => await synchrony.deobfuscateSource(x)
+//const checkDeobfs = (x) => x.indexOf("encrypted") !== -1
 
 async function getDeobfuscatedScript() {
     const vidplayHost = "https://thetvapp.to/"
@@ -42,33 +39,36 @@ async function getDeobfuscatedScript() {
 
     //#https://thetvapp.to/build/assets/app-6a4b6fce.js
     const scriptUrl = `https://thetvapp.to/build/assets/${codeVersion}`
-    console.info(scriptUrl)
+	
 	
 	
 
     const obfuscatedScript = await fetch(scriptUrl, {headers: headers}).then(async (x) => await x.text())
+	
+	
+	
 
-    const firstTry = await deobfuscationChain(obfuscatedScript, [synchronyStep, synchronyStep])
-  //  if (checkDeobfs(firstTry)) return firstTry
+    const firstTry = await deobfuscationx(obfuscatedScript)
+    return firstTry
 
-    const secondTry = await deobfuscationChain(obfuscatedScript, [synchronyStep])
-    return secondTry
+    //const secondTry = await deobfuscationx(obfuscatedScript, [synchronyStep])
+    //return secondTry
 }
 
 const deobfuscated = await getDeobfuscatedScript() 
 
 // Phase 4: Let's find the keys!
-if (checkDeobfs(deobfuscated)) {
-   // const start = deobfuscated.substring(deobfuscated.indexOf("<video />"))
-   // const end = start.substring(0, start.indexOf(".replace"))
-   // const keys = Array.from(end.matchAll(/'(\w+)'/g), x => x[1])
-   // assert(keys.length == 2, "Invalid array length!")
+//if (checkDeobfs(deobfuscated)) {
+//    const start = deobfuscated.substring(deobfuscated.indexOf("<video />"))
+//    const end = start.substring(0, start.indexOf(".replace"))
+//    const keys = Array.from(end.matchAll(/'(\w+)'/g), x => x[1])
+//    assert(keys.length == 2, "Invalid array length!")
 
     // Be happy!
-    console.info("Success!")
-    await writeFile("keys.json", deobfuscated, "utf8")
-} else {
-    // ... Or not xD
-    console.error("FAIL!")
-    await writeFile("failed.js", deobfuscated, "utf8")
-}
+console.info("Success!")
+await writeFile("keys.json", deobfuscated, "utf8")
+//} else {
+//    // ... Or not xD
+//    console.error("FAIL!")
+//    await writeFile("failed.js", deobfuscated, "utf8")
+//}
